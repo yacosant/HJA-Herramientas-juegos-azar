f4a9d0544e092398d31ffcb0f36cb55ac96079a0
Yaco <yacosant>
2016-10-11 16:28:53 +0200

    Estructura general y dos metodos
    Está comentado en el codigo

--- hja/src/hja/Logica.java
+++ hja/src/hja/Logica.java
@@ -10,7 +10,24 @@ package hja;
  * @author YVCX
  */
 public class Logica {
-    void foo(){
+    
+    private Carta[] cartas;
+    private int cont=0;
+    private String[] manos = new String[9];
+    public void setCartas(Carta[] cartas) {
+        this.cartas = cartas;
+    }
+        
+    void comprobar(){
+        
+        //aqui s ehacen todas las llamadas a cada uno de los metodos que comprueban una mano.
+        //En el array de string se guardan las manos que si se han producido, y en funcion de si se han producido, 
+        //el comprobar s eocmprobaran los menores o no, ejemplo: si se han ecncontrado trio no se comprobará pareja
+        
+       //----
+       //Lamadas a:
+       
+       
        // straight flush(escalerade color)
        
        // four-of-a-kind (or quads) (poker)
@@ -31,6 +48,80 @@ public class Logica {
         
     }
            
-    
+    String escaleraColor(){
+        String mano= "";
+        
+        return mano;
+    }
    
+    String poker(){
+        String mano= "";
+        
+        return mano;
+    }
+    
+    String full(){
+        String mano= "";
+        
+        return mano;
+    }
+    
+    String color(){
+        String mano= "";
+        
+        return mano;
+    }
+       
+     String escalera(){
+        String mano= "";
+        
+        return mano;
+    }
+     
+    String trio(){
+        String mano= "";
+        
+        return mano;
+    }
+    
+    String doblePareja(){
+        String mano= "";
+        
+        return mano;
+    }
+    
+    void pareja(){
+        String mano= "Pareja";
+        boolean cierto= false;
+        int i=0, j=0;
+        
+        while( i<5 && !cierto){
+           char carta= cartas[i].getValor();
+           
+           while( j<5 && !cierto){
+               if(i!=j){
+                   if(i==j)cierto=true;
+                   manos[cont]="Pareja";
+                   cont++;
+               }
+               j++;
+           }
+           }
+           i++;
+        }
+         
+    void cartaAlta(){
+        boolean cierto =false;
+        int i=0;
+        
+        while( i<5 && !cierto){
+           char c= cartas[i].getValor();
+           if(c=='A' || c=='K' || c=='Q' || c=='J'){
+               cierto=true;
+               manos[cont]="Carta Alta";
+               cont++;
+           }
+           i++;
+        }
+    }
 }

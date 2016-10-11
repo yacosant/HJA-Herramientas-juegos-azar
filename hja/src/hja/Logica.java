9dc3909f5d8859be80dc506fe03e4a3c4fde5e92
Yaco <yacosant>
2016-10-11 15:45:52 +0200

    Creada clase Logica donde compararemos las cosas

---
+++ hja/src/hja/Logica.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hja;
+
+/**
+ *
+ * @author YVCX
+ */
+public class Logica {
+    void foo(){
+       // straight flush(escalerade color)
+       
+       // four-of-a-kind (or quads) (poker)
+       
+       // full house (or boat) (full)
+       
+       // flush(color)
+       
+       // straight(escalera)
+       
+       // three-of-a-kind(trío)
+       
+       // two-pair(doblepareja)
+       
+       // pair (parejao par)
+       
+        //high card (carta alta)
+        
+    }
+           
+    
+   
+}

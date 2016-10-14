aa6508ee4a2889f75ba6cb3ac532909043a6c3a1
Yaco <yacosant>
2016-10-14 18:06:54 +0200

    quitado dobleFor de escalera

--- hja/src/hja/Logica.java
+++ hja/src/hja/Logica.java
@@ -88,6 +88,25 @@ public class Logica {
 		return esEscalera;
 	}
 
+        boolean escaleraSinDobleFor(Carta[] cartas) {
+		boolean esEscalera = false,posible = true;
+		ordenador(cartas);
+		int cont;
+		for (int i = 0; i < cartas.length && posible; i++) {
+			cont = 0;
+			
+                        if(cartas[i].getValor() == 13 && cartas[i+1].getValor() == 2)//el unico caso distinto,considerando as como 13
+                                cont++;
+                        else if(cartas[i].getValor()+1 == cartas[i+1].getValor())
+                                cont++;
+                        else posible = false;
+
+                        if (cont == 4) esEscalera = true;
+		
+		}
+		return esEscalera;
+	}
+        
 	boolean poker(Carta[] cartas) {
 		boolean esPoker = false;
 		int cont = 0;

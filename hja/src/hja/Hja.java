209b1a446f1f6fc211f83ac4e76869cbec1a6d47
damasilovic <damasa01@ucm.es>
2016-10-15 18:23:00 +0200

    Main con comando para ver que juego poner
    hay una cosa que no se hacer aun(toy meditando)

--- hja/src/hja/Hja.java
+++ hja/src/hja/Hja.java
@@ -11,7 +11,88 @@ public class Hja {
 	 * @param args
 	 *            the command line arguments
 	 */
+
+	enum GameInfo { //Enumerado de los juegos que tenemos
+		OMAHA("om", "Omaha"),
+		CincoCartas("cincoC", "CincoCartas"),
+		DosCartas("dos", "DosCartas"),
+		NJugadores("nJug","Njugadores");
+
+		private String id;
+		private String desc;
+
+		GameInfo(String id, String desc) {
+			this.id = id;
+			this.desc = desc;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public String getDesc() {
+			return desc;
+		}
+
+		@Override
+		public String toString() {
+			return id;
+		}
+
+	}
+
+	//Creamos el comando. Con -g metemos que juego queremos
+	private static Option constructGameOption() { 
+		String optionInfo = "The game to play ( ";
+		for (GameInfo i : GameInfo.values()) {
+			optionInfo += i.getId() + " [for " + i.getDesc() + "] ";
+		}
+		optionInfo += "). By defualt, " + DEFAULT_GAME.getId() + ".";
+		Option opt = new Option("g", "game", true, optionInfo);
+		opt.setArgName("game identifier");
+		return opt;
+	}
+
+
+	//Parseamos el comando del juego que queremos jugar
+	private static void parseGameOption(CommandLine line) throws ParseException {
+		String gameVal = line.getOptionValue("g", DEFAULT_GAME.getId());
+		GameInfo selectedGame = null;
+
+		for (GameInfo g : GameInfo.values()) {
+			if (g.getId().equals(gameVal)) {
+				selectedGame = g;
+				break;
+			}
+		}
+
+		if (selectedGame == null) {
+			throw new ParseException("Uknown game '" + gameVal + "'");
+		}
+
+		//Switch de los juegos que hay 
+		switch ( selectedGame ) {
+		case OMAHA:
+			gameFactory = new OmahaFactory();//HABRA QUE CREARLAS O PONER AQUI DIRECTAMENTE LAS DIFERENCIAS
+			break;
+		case CincoCartas:
+			gameFactory = new CincoCartasFactory();
+			break;
+		case DosCartas:
+			gameFactory = new DosCartasFactory();
+			break;
+		case NJugadores:
+			gameFactory = new NJugadoresFactory();
+		}
+
+	}
+
 	public static void main(String[] args) {
+		
+		//Game g = gameFactory.u(); meditando como lo hare ..
+
+		//createSwingView(g); cuando hagamos swing view necesitaremos pasarle el juego para mostrar uno u otro
+
 		// leemos archivo
 		MejorJugada ar = new MejorJugada();
 		Logica log = new Logica();
@@ -24,5 +105,4 @@ public class Hja {
 			cont++;
 		}
 	}
-
 }

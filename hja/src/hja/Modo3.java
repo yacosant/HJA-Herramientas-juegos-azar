b662defa5118c7c845e04bd5dfdf0711ed9f8446
Yaco <yacosant>
2016-10-21 12:25:45 +0200

    .

--- hja/src/hja/Modo3.java
+++ hja/src/hja/Modo3.java
@@ -14,7 +14,8 @@ import java.util.ArrayList;
 public class Modo3 {
     private ArrayList<Carta> cartas;
     private int peso;
-
+    private int jugador;
+    
     public ArrayList<Carta> getCartas() {
         return cartas;
     }
@@ -29,6 +30,14 @@ public class Modo3 {
 
     public void setPeso(int peso) {
         this.peso = peso;
+    }
+
+    public int getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(int jugador) {
+        this.jugador = jugador;
     }
     
 }

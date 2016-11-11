6e18873b3be2f27f7c387998cfd12d39f0fbe884
Yaco <yacosant>
2016-11-11 20:57:45 +0100

    gui pr2.1

---
+++ hja/src/guihja/Carta.java
@@ -0,0 +1,30 @@
+package guihja;
+
+/**
+ *
+ * @author Grupo 1
+ *
+ */
+public class Carta {
+
+    private char color;
+    private int valor;
+
+    public Carta(int val, char col) {
+        valor = val;
+        color = col;
+    }
+
+    public char getColor() {
+        return color;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int v) {
+        valor = v;
+    }
+
+}
---
+++ hja/src/guihja/LogicaGui.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guihja;
+
+/**
+ *
+ * @author YVCX
+ */
+public class LogicaGui {
+       
+    public static String intToChar(int valor) {
+        String carta = null;
+        switch (valor) {
+            case 10:
+                carta = "T";
+                break;
+            case 11:
+                carta = "J";
+                break;
+            case 12:
+                carta = "Q";
+                break;
+            case 13:
+                carta = "K";
+                break;
+            case 14:
+                carta = "A";
+                break;
+            default:
+                carta = Integer.toString(valor);
+                break;
+        }
+        return carta;
+    }
+
+    public static int CharToInt(char valor) {
+        int carta;
+        switch (valor) {
+            case 'T':
+                carta = 10;
+                break;
+            case 'J':
+                carta = 11;
+                break;
+            case 'Q':
+                carta = 12;
+                break;
+            case 'K':
+                carta = 13;
+                break;
+            case 'A':
+                carta = 14;
+                break;
+             case 't':
+                carta = 10;
+                break;
+            case 'j':
+                carta = 11;
+                break;
+            case 'q':
+                carta = 12;
+                break;
+            case 'k':
+                carta = 13;
+                break;
+            case 'a':
+                carta = 14;
+                break;
+            default:
+                carta = Character.getNumericValue(valor);
+                break;
+        }
+        return carta;
+    }
+
+}

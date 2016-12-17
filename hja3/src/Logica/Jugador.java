cc22c3a437decc6fa6b6c45e5282ab098485de6b
JoniSanchez <jonisanchezparedes@gmail.com>
2016-12-17 15:45:22 +0100

    jugdador y jugadores para juntar vista y logica

---
+++ hja3/src/Logica/Jugador.java
@@ -0,0 +1,11 @@
+package Logica;
+
+public class Jugador {
+	private Carta carta1;
+	private Carta carta2;
+
+	public Jugador(Carta carta1, Carta carta2) {
+		this.carta1 = carta1;
+		this.carta2 = carta2;
+	}
+}
---
+++ hja3/src/Logica/Jugadores.java
@@ -0,0 +1,12 @@
+package Logica;
+
+import java.util.ArrayList;
+
+public class Jugadores {
+	
+	private ArrayList<Jugador> jugadores = new ArrayList<>();
+	
+	public Jugadores(Jugador juga){
+		jugadores.add(juga);
+	}
+}
